240a85282a2f75b16632a81a1f922504abfdbc3c
phuocbuinhu <phuoc.bui.yg@renesas.com>
2024-11-22 11:10:25 +0700

    completed watch log by loki

--- src/main/java/org/example/booklibrary/service/Impl/AuthorServiceImpl.java
+++ src/main/java/org/example/booklibrary/service/Impl/AuthorServiceImpl.java
@@ -3,6 +3,8 @@ package org.example.booklibrary.service.Impl;
 import org.example.booklibrary.entity.Author;
 import org.example.booklibrary.repository.AuthorRepository;
 import org.example.booklibrary.service.AuthorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -12,6 +14,7 @@ import java.util.Optional;
 public class AuthorServiceImpl implements AuthorService {
 
   private AuthorRepository authorRepository;
+  private static final Logger logger = LoggerFactory.getLogger(AuthorServiceImpl.class);
 
   @Autowired
   public AuthorServiceImpl(AuthorRepository authorRepository) {
@@ -20,22 +23,37 @@ public class AuthorServiceImpl implements AuthorService {
 
   @Override
   public List<Author> getAllAuthors() {
-    return authorRepository.findAll();
+    logger.info("Fetching all authors");
+    List<Author> authors = authorRepository.findAll();
+    logger.info("Fetched {} authors", authors.size());
+    return authors;
   }
 
   @Override
   public Optional<Author> getAuthorById(Long id) {
-    return authorRepository.findById(id);
+    logger.info("Fetching author with ID: {}", id);
+    Optional<Author> author = authorRepository.findById(id);
+    if (author.isPresent()) {
+      logger.info("Author found: {}", author.get());
+    } else {
+      logger.warn("No author found with ID: {}", id);
+    }
+    return author;
   }
 
   @Override
   public Author saveAuthor(Author author) {
-    return authorRepository.save(author);
+    logger.info("Saving author: {}", author);
+    Author savedAuthor = authorRepository.save(author);
+    logger.info("Author saved: {}", savedAuthor);
+    return savedAuthor;
   }
 
   @Override
   public void deleteAuthor(Long id) {
+    logger.info("Deleting author with ID: {}", id);
     authorRepository.deleteById(id);
+    logger.info("Deleted author with ID: {}", id);
   }
 
   @Override
